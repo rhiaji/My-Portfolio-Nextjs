d286504f551c2c9eed8f2e28506322610811c6b9
rhiaji <mrdavemariano@gmail.com>
2023-11-03 01:26:07 +0800

    first commit

--- public/components/about.js
+++ public/components/about.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from "../../public/css/styles.module.css";
+import React from "react"
+import styles from "../../public/css/styles.module.css"
 
 const About = React.forwardRef((props, ref) => {
   return (
@@ -8,38 +8,31 @@ const About = React.forwardRef((props, ref) => {
         <h2>About Me</h2>
       </div>
       <div className={styles.sectionContent}>
-        <div>
+        <div className={styles.aboutMe}>
           <p>
-            Hello, I'm Dave Mariano, a dedicated and self-taught web developer
-            with a strong commitment to continuous learning and web application
-            development. Despite being a registered mechanical engineer, I am
-            deeply passionate about honing my programming skills and
-            contributing to the world of web development.
+            Hello, I'm Dave Mariano, a dedicated and self-taught web developer with a strong commitment to continuous learning and web application
+            development. Despite being a registered mechanical engineer, I am deeply passionate about honing my programming skills and contributing to
+            the world of web development.
           </p>
           <p>
-            My expertise encompasses a range of essential technologies,
-            including JavaScript, Html, Css, Node.js, Express, React, Firebase,
-            and MongoDB. I have a keen interest in blockchain technology,
-            particularly with a focus on Hive blockchain. Additionally, I am
-            eager to explore various aspects of web development to further
-            refine my skills
+            My expertise encompasses a range of essential technologies, including JavaScript, Html, Css, Node.js, Express, React, Firebase, and
+            MongoDB. I have a keen interest in blockchain technology, particularly with a focus on Hive blockchain. Additionally, I am eager to
+            explore various aspects of web development to further refine my skills
           </p>
           <p>
-            My portfolio reflects my commitment to creating innovative web
-            solutions. Notable projects include the development of an online
-            book-making platform, designed to foster collaborative storytelling,
-            as well as a real-time multiplayer tic-tac-toe game seamlessly
+            My portfolio reflects my commitment to creating innovative web solutions. Notable projects include the development of an online
+            book-making platform, designed to foster collaborative storytelling, as well as a real-time multiplayer tic-tac-toe game seamlessly
             integrated with Firebase.
           </p>
         </div>
 
         <div className={styles.imageContainer}>
           <img src="/images/MYPIC.png" />
-          Registered Mechanical Engineer
+          <p>Registered Mechanical Engineer</p>
         </div>
       </div>
     </section>
-  );
-});
+  )
+})
 
-export default About;
+export default About
